fdd85bfddec767437f756f932af11c37532be27e
Rafael Frecceiro Santos <rafa.frecceiro@gmail.com>
2017-10-27 00:15:21 -0200

    criando box para comentários

--- index.php
+++ index.php
@@ -7,6 +7,11 @@
 		<script type="text/javascript" src="jquery.min.js"></script>
 		<script type="text/javascript" src="jquery.flipcountdown.js"></script>
 		<link rel="stylesheet" type="text/css" href="jquery.flipcountdown.css" />
+
+        <link rel="stylesheet" type="text/css" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" />
+        <script type="text/javascript" src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
+
+
 		<script>
 			jQuery(function($){
 				var i = 1000;
@@ -38,7 +43,7 @@
                                             <td style="width:105px;text-align:center;" class="dias">Dias</td>
                                             <td style="width:95px;text-align:center;" class="dias">Horas</td>
                                             <td style="width:58px;text-align:center;" class="dias">Minutos</td> 
-                                            <td style="width:95px;text-align:center;" class="dias">Segundos</td>
+                                            <td style="width:95px;text-align:center;float:left" class="dias">Segundos</td>
                                     </tr>
                                     <tr>
                                             <td colspan="4"><span id="new_year"></span></td>
@@ -77,15 +82,59 @@
 				#main{
 					margin:0px auto; 
 					padding:0px; 	
-					width: 360px;
+					width: 420px;
 					position: relative;
 				}					
                                 
-                                #contagem
-                                {                                   
-                                    margin: 0px auto;
-                                }
+                #contagem
+                {
+                    margin: 0px auto;
+                }
+
+                .inner{
+
+                    float: left;
+                    width: 100%;
+
+                    width: 400px;
+                    height: 200px;
+                    background-color: white;
+                    margin-left: 50px;
+                    margin-top: 15px;
+                    border-radius: 10px;
+                    border: 1px solid gray;
+                }
+
+                @media (min-width:100%){
+                    .inner{
+                        width: 50%;
+                    }
+
 			</style>
 		</div>
+        
+        <?php
+            $con = mysqli_connect("br52.hostgator.com.br","rafae415_user","admin","rafae415_rafaelmonica");
+            $sql = "select * from depoimentos";
+            $res = mysqli_query($con,$sql)->fetch_assoc();
+
+        ?>
+
+    <div style="width: 100%;">
+
+        <?php
+        foreach ($res as $key => $value) {
+                echo '<div class="inner">
+                        <div style="100%;height: 30px; border-bottom: 2px solid gray;"></div>
+                    </div>';
+            }
+        ?>
+    </div>
+
+    <div class="clearfix"></div>
+    <div style="text-align: center; margin-top: 50px">
+        <input type="text" style="width: 300px;"/>
+        <input type="button" value="Salvar"/>
+    </div>
 	</body>
 </html>
